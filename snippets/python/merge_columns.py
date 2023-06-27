f956c89a692b91d4e31e756fba5826ffdc7a4621
Yashpal Ramakrishnaiah <yashpal.ramakrishnaiah1@monash.edu>
2023-06-27 16:33:10 +1000

    updated merging logic + updated test file

--- snippets/python/merge_columns.py
+++ snippets/python/merge_columns.py
@@ -1,17 +1,14 @@
 import pandas as pd
 
 
-def merge_columns(*merge_columns, time_stamp_column, input_file_name, output_column_name='merged', output_file_name='merged.csv'):
+def merge_columns(*merge_columns, input_file_name, output_column_name='merged', output_file_name='merged.csv'):
     df = pd.read_csv(input_file_name)
     columns = list(merge_columns)
-    columns.append(time_stamp_column)
-    df = df[columns]
-    df = df.groupby(time_stamp_column).agg('mean').reset_index()
-    out_df = pd.DataFrame(df[time_stamp_column])
-    out_df[output_column_name] = df[list(merge_columns)].mean(axis=1)
-    out_df = out_df.dropna()
-    out_df.to_csv(output_file_name, index=False)
+    df[output_column_name] = df[columns].mean(axis=1)
+    df.drop(columns, axis=1, inplace=True)
+    df = df.dropna()
+    df.to_csv(output_file_name, index=False)
 
 
 if __name__ == '__main__':
-    merge_columns('col2', 'col3', time_stamp_column = 'col4', input_file_name='snippets/python/data/testing.csv', output_column_name='merged_col', output_file_name='snippets/python/data/merged.csv')
+    merge_columns('col2', 'col3', 'col4', input_file_name='snippets/python/data/testing.csv', output_column_name='merged_col', output_file_name='snippets/python/data/merged.csv')
