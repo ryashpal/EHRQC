eeb90fbd58ab82a183d4ee5b600ee8ff34043548
Yashpal Ramakrishnaiah <yashpal.ramakrishnaiah1@monash.edu>
2023-07-04 12:25:10 +1000

    automate merging for multiple column groups

--- snippets/python/merge_columns.py
+++ snippets/python/merge_columns.py
@@ -1,13 +1,18 @@
 import pandas as pd
 
 
-def merge_columns(*merge_columns, input_file_name, output_column_name='merged', output_file_name='merged.csv'):
-    df = pd.read_csv(input_file_name)
+def _merge_columns(*merge_columns, input_df, output_column_name='merged'):
     columns = list(merge_columns)
-    df[output_column_name] = df[columns].mean(axis=1)
-    df.drop(columns, axis=1, inplace=True)
+    input_df[output_column_name] = input_df[columns].mean(axis=1)
+    input_df.drop(columns, axis=1, inplace=True)
+
+
+def merge_columns(column_mapping, input_file_name, output_file_name='merged.csv'):
+    df = pd.read_csv(input_file_name)
+    for key in column_mapping.keys():
+        _merge_columns(*column_mapping[key], input_df=df, output_column_name=key)
     df.to_csv(output_file_name, index=False)
 
 
 if __name__ == '__main__':
-    merge_columns('col2', 'col3', 'col4', input_file_name='snippets/python/data/testing.csv', output_column_name='merged_col', output_file_name='snippets/python/data/merged.csv')
+    merge_columns({'merged_col_1' : ['col1', 'col2'], 'merged_col_2' : ['col3', 'col4']}, input_file_name='snippets/python/data/testing.csv', output_file_name='snippets/python/data/merged.csv')
