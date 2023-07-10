4a62e7f2fbf4a5ed955e7db636060560b1f27d83
Yashpal Ramakrishnaiah <yashpal.ramakrishnaiah1@monash.edu>
2023-07-10 12:50:26 +1000

    updated missingness script to drop na columns

--- snippets/python/calculate_missingness.py
+++ snippets/python/calculate_missingness.py
@@ -1,9 +1,12 @@
 def calculate_missingness_for_large_file():
     import pandas as pd
+    import numpy as np
 
     chunksize=100
     missing_df_list = []
     for df in pd.read_csv("all_obs.csv",  chunksize=chunksize):
+        df.replace('', np.nan, inplace=True)
+        df = df.dropna(subset=df.columns[~df.columns.isin(['PATIENT_ID', 'EPISODE_ID', 'RESULT_UPDT_DATETIME'])], how='any')
         missing_counts = df.isnull().sum()
         missing_df = pd.DataFrame({'column_name': df.columns, 'missing_count': missing_counts, 'total_count': len(df.columns)*[len(df)]})
         missing_df_list.append(missing_df)
