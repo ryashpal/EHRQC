4e97956d7bd3b6a3ae9b46f3b5620abc1040aae3
Yashpal Ramakrishnaiah <yashpal.ramakrishnaiah1@monash.edu>
2023-08-28 21:22:34 +1000

    sorting missing values

--- ehrqc/qc/Anomalies.py
+++ ehrqc/qc/Anomalies.py
@@ -74,7 +74,7 @@ def detect(
                         doc.asis('<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/></svg>')
                         with tag('span', klass='fs-4', style="margin: 10px;"):
                             text('Missing Data Analysis')
-                    drawMissingAnalysis(df, df.columns, tag, text)
+                    drawMissingAnalysis(df, tag, text)
                 doc.asis('<div style="clear:both;"></div>')
             if outliers:
                 # for (x, y) in itertools.combinations(df.columns, 2):
@@ -217,7 +217,7 @@ def correct(
     outliersDf.to_csv(outputFile, index=False)
 
 
-def drawMissingAnalysis(df, colNames, tag, text):
+def drawMissingAnalysis(df, tag, text):
 
     with tag('table table-dark', style='border: 1px solid black; border-collapse: collapse'):
         with tag('tr'):
@@ -227,20 +227,22 @@ def drawMissingAnalysis(df, colNames, tag, text):
                 text('Count')
             with tag('th', style='border: 1px solid black; border-collapse: collapse'):
                 text('Percentage')
+
         isMissing = False
-        for col in colNames:
-            if col in df.columns:
-                with tag('tr'):
-                    with tag('td', style='border: 1px solid black; border-collapse: collapse'):
-                        text(col)
-                    missingCount = df[col].isna().sum()
-                    missingProportion = round(df[col].isna().sum()/df[col].count()*100, 2)
-                    if missingCount > 0:
-                        isMissing = True
-                    with tag('td', style='border: 1px solid black; border-collapse: collapse'):
-                        text(str(missingCount))
-                    with tag('td', style='border: 1px solid black; border-collapse: collapse'):
-                        text(str(missingProportion))
+
+        missingDf = df.isna().sum().sort_values()
+
+        for (index, value) in missingDf.items():
+            with tag('tr'):
+                with tag('td', style='border: 1px solid black; border-collapse: collapse'):
+                    text(index)
+                missingProportion = round(value/df[index].count()*100, 2)
+                if value > 0:
+                    isMissing = True
+                with tag('td', style='border: 1px solid black; border-collapse: collapse'):
+                    text(str(value))
+                with tag('td', style='border: 1px solid black; border-collapse: collapse'):
+                    text(str(missingProportion))
 
     if isMissing:
         with tag('div'):
